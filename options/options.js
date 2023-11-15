d437565d495edd1bb6567083867ca8a13f1b9c55
Husnain Nasir <husnain.nasir@crowdbotics.com>
2023-11-15 12:40:27 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "dummy-project-44530",
-  url: "https://dummy-project-44530.botics.co",
-  api: "https://dummy-project-44530.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "dummy-project-44530",
+  "url": "https://dummy-project-44530.botics.co",
+  "api": "https://dummy-project-44530.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
