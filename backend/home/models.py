8e02fdd1750265672e9a04a550092fa2bf888c2e
crowdbotics-apps <team@crowdbotics.com>
2023-11-28 09:17:21 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -4,11 +4,24 @@ from django.db import models
 
 class Expense(models.Model):
     "Generated Model"
-    expenseName = models.BigIntegerField()
     user_id = models.OneToOneField(
         "users.User",
-        on_delete=models.CASCADE,
         null=True,
         blank=True,
+        on_delete=models.CASCADE,
         related_name="expense_user_id",
+    )
+    name = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    amount = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    time = models.DateTimeField(
+        auto_now=True,
+        null=True,
+        blank=True,
     )
